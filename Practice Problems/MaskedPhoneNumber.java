86475bd38748fb3e48e4888768ae501b3fa605ad
adityasingh-07hub <adityaaingh2007@gmail.com>
2026-09-05 07:24:22 +0530

    Add files via upload

---
+++ Practice Problems/MaskedPhoneNumber.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class MaskedPhoneNumber {
+
+    static String maskPhoneNumber(String phone) {
+
+        if(phone.length()!=10)
+            return "Invalid phone number";
+
+        for(int i=0;i<phone.length();i++) {
+
+            if(!Character.isDigit(phone.charAt(i)))
+                return "Invalid phone number";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("XXXXXX");
+        sb.append("-");
+        sb.append(phone.substring(6));
+
+        return sb.toString();
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter phone number: ");
+        String phone = sc.nextLine();
+
+        System.out.println(maskPhoneNumber(phone));
+
+        sc.close();
+    }
+}
